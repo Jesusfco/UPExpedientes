24cc4a42b4a62c76d2744aa9d8983cb3f62b873f
Jesús Fco Cortés <jfcr@live.com>
2018-04-14 21:22:06 -0500

    crud recete start

---
+++ menuroot2/recetas/menu.php
@@ -0,0 +1,7 @@
+<a href="">Crear Usuario</a><br>
+<a href="expedientes/create.php">Crear Expedientes</a><br>
+<a href="recetas/create.php">Crear Receta</a><br>
+<a href="recetas">Expedientes</a><br>
+        
+<br><br><br>
+<a href="php/closeSession.php">Salir</a>
