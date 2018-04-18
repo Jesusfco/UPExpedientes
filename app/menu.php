b687d270255750bfe6569472330225c2c55bd65f
Jesús Fco Cortés <jfcr@live.com>
2018-04-18 17:25:02 -0500

    changes menu app

--- app/menu.php
+++ app/menu.php
@@ -1,21 +1,14 @@
 <a href="index.php">USUARIOS</a>
 <a href="recetas/">RECETAS</a>
 <a href="orden/"> ORDEN DE RECETA</a>
-<<<<<<< HEAD
 <a href="muestras/">MUESTRASDE LABORATORIO</a>
 <a href="alergias/">ALERGIAS</a>
 <a href="historial/">HISTORIAL</a>
 <a href="citas/">CITAS</a>
 <a href="casos/">CASOS NOTIFICABLES</a>
 <a href="auxiliar/">RESUMEN CLÍNICO</a>
-=======
-<a href="muestras/">MUESTRAS DE LABORATORIO</a>
-<a href="alergias/">ALERGIAS</a>
-<a href="historial/">HISTORIAL</a>
-<a href="citas/">CITAS</a>
-<a href="casos/">RESUMEN CLÍNICO</a>
-<a href="auxiliar/">AUXILIAR DE DIAGNOSTICO</a>
->>>>>>> 27f3e984252e76b06b8e13fa3ac28af871bab733
+
+
 
         
 <br><br>
